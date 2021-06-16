7640d1e481062c9877ca6ddb592fc01d38c54014
sai <saikittu987.sk@gmail.com>
2021-06-16 15:44:29 +0530

    commit

---
+++ Level-1/src/GettingStarted/a.java
@@ -0,0 +1,10 @@
+package GettingStarted;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Level-1/src/GettingStarted/sai/hai.java
@@ -0,0 +1,10 @@
+package GettingStarted.sai;
+
+public class hai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
